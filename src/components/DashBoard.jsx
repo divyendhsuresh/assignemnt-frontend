fbe58da4816d119466182637152f86397ff9fed2
divyendh <159880316+divyendh@users.noreply.github.com>
2024-09-03 17:02:10 +0530

    added dropdown and integrated api for user select

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 import DropDown from "./DropDown";
 
 const DashBoard = () => {
-  const [username, setUsername] = useState("user2");
+  const [username, setUsername] = useState(undefined);
   const [page, setPage] = useState(2);
 
   const { data: homes, isFetching: isHomeLoading } = useFetchAllHomes(
@@ -15,23 +15,32 @@ const DashBoard = () => {
   if (isHomeLoading) {
     return <div>homes data loading</div>;
   }
+
+  console.log(homes);
+
   return (
     <div className="maxW-100% p-2">
-      <DropDown />
-      <div className="grid md:grid-cols-4 grid-cols-2 gap-2">
-        {homes.map((home, index) => (
-          <Card
-            sqft={home.sqft}
-            streetAddress={home.street_address}
-            state={home.state}
-            zip={home.zip}
-            beds={home.beds}
-            baths={home.baths}
-            listPrice={home.list_price}
-            key={index}
-          />
-        ))}
+      <div className="flex justify-end">
+        <DropDown setUsername={setUsername} />
       </div>
+      {!homes || homes?.length === 0 ? (
+        <div>no data to show </div>
+      ) : (
+        <div className="grid md:grid-cols-4 grid-cols-2 gap-2">
+          {homes.map((home, index) => (
+            <Card
+              sqft={home.sqft}
+              streetAddress={home.street_address}
+              state={home.state}
+              zip={home.zip}
+              beds={home.beds}
+              baths={home.baths}
+              listPrice={home.list_price}
+              key={index}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,8 +1,14 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { useFetchAllUsers } from "../apis/useUsers";
+import { useEffect, useState } from "react";
 
-export default function DropDown() {
+export default function DropDown({ setUsername }) {
   const { data, isLoading, isError } = useFetchAllUsers();
+  const [selectedItem, setSelectedItem] = useState();
+
+  useEffect(() => {
+    console.log(selectedItem);
+  }, [selectedItem]);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading users</div>;
@@ -11,18 +17,23 @@ export default function DropDown() {
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Options
+          Users
         </MenuButton>
       </div>
 
       <MenuItems
         transition
-        className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+        className="absolute right-0 z-10 mt-2  rounded-md bg-white shadow-lg px-4 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
       >
-        <div className="py-1">
+        <div className="p-2">
           {data.map((user) => (
             <MenuItem key={user.email}>
-              <div>{user.username}</div>
+              <div
+                onClick={() => setUsername(user.username)}
+                className="hover:bg-red z-12"
+              >
+                {user.username}
+              </div>
             </MenuItem>
           ))}
         </div>
